be7485471212b5ce68a86e6e268aa73eee086e50
baehyenn <ttottottotto1129@gmail.com>
2025-11-14 09:01:00 +0900

    FirstUpload입니다.

---
+++ testWithH/src/testWithH/test235.java
@@ -0,0 +1,10 @@
+package testWithH;
+
+public class test235 {
+	public static void main(String[] args) {
+		System.out.println("안녕ㅎ;");
+		System.out.println("안녕ㅎ;");
+		System.out.println("안녕ㅎ;");
+		System.out.println("안녕ㅎ;");
+	}
+}
